bf3452fe675e7838bb90d09fbbed8e96f9acac97
Timothy J. Wood <tjw@omnigroup.com>
2015-09-13 21:57:26 -0700

    Cleanup a bit.

--- binary-search.swift
+++ binary-search.swift
@@ -1,18 +1,14 @@
 
-enum BinarySearchResult<T> {
+// Sadly, can't add an enum inside an extension, or this would be better inside Indexable
+enum SearchResult<T> {
     case Missing(insertIndex:T)
     case Found(index:T)
 }
 
-protocol BinarySearchable {
-    typealias Index: ForwardIndexType
-    typealias Element: Comparable
-    func binarySearch(item:Element) -> BinarySearchResult<Index>
-}
-
 // Add this to Indexable so that we get it on Array<T>, ArraySlice<T>, and ContiguousArray<T>
 extension Indexable where Self._Element: Comparable {
-    func binarySearch(item:Self._Element) -> BinarySearchResult<Self.Index> {
+    
+    func binarySearch(item:Self._Element) -> SearchResult<Self.Index> {
         var left = self.startIndex
         var right = self.endIndex
         
@@ -20,7 +16,7 @@ extension Indexable where Self._Element: Comparable {
             let mid = left.advancedBy(left.distanceTo(right) / 2)
             
             if self[mid] == item {
-                return BinarySearchResult.Found(index:mid)
+                return SearchResult.Found(index:mid)
             }
             if self[mid] < item {
                 left = mid.advancedBy(1)
@@ -30,7 +26,7 @@ extension Indexable where Self._Element: Comparable {
         }
         
         // left == right is a zero length range
-        return BinarySearchResult.Missing(insertIndex:left)
+        return SearchResult.Missing(insertIndex:left)
     }
 }
 
